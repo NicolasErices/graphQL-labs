23be4ff17626d403e755698feb2042ec74d51689
carlos erices mendez <carlos.erices@bci.cl>
2020-05-18 23:33:07 -0400

    firs commit | GraphQl Labs demo

---
+++ index.js
@@ -0,0 +1,22 @@
+'use stric'
+
+const { makeExecutableSchema } = require('graphql-tools');
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolver')
+
+const app = express();
+const port = process.env.port || 3000
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'),'utf-8')
+const schema = makeExecutableSchema({typeDefs, resolvers})
+
+app.use('/api', gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+}))
+app.listen(port, ()=>{
+    console.log(`server is listening ${port}/api`)
+})
